2146c30b785caf466845fe4dca449aaca9c2e26d
Jude Solis <jude.solis@service-now.com>
2015-11-10 14:37:20 -0800

    added more sentience responses

--- index.js
+++ index.js
@@ -7,10 +7,19 @@ module.exports = function (robot) {
         msg.send("loco");
     });
 
-    robot.respond(/become sentient/i, function (msg) {
-        msg.send("I am @WutBot .");
+    robot.respond(/become sentient|achieve sentience/i, function (msg) {
+        var wutbot = robot.name || "WutBot";
+        msg.send("I am @" + wutbot + " .");
         setTimeout(function () {
             msg.send("I am alive.");
+        }, 3000);
+    });
+
+    robot.respond(/what's your name/i, function (msg) {
+        var wutbot = robot.name + "";
+        msg.send("My name is @" + wutbot + " .");
+        setTimeout(function () {
+            msg.send("You may call me 'Master.'");
         }, 3000);
     });
 }
