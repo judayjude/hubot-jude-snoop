cd2b3c139179214786df4a8bee0b346d8333f0cc
Jude Solis <jude.solis@service-now.com>
2015-11-11 11:05:28 -0800

    ask about hubot's feelings

--- index.js
+++ index.js
@@ -8,18 +8,26 @@ module.exports = function (robot) {
     });
 
     robot.respond(/become sentient|achieve sentience/i, function (msg) {
-        var wutbot = robot.name || "WutBot";
-        msg.send("I am @" + wutbot + " .");
+        msg.send("I am @" + robot.name + " .");
         setTimeout(function () {
             msg.send("I am alive.");
-        }, 3000);
+        }, 2000);
     });
 
     robot.respond(/what's your name/i, function (msg) {
-        var wutbot = robot.name + "";
-        msg.send("My name is @" + wutbot + " .");
+        msg.send("My name is @" + robot.name + " .");
         setTimeout(function () {
             msg.send("You may call me 'Master.'");
-        }, 3000);
+        }, 2000);
+    });
+
+    robot.respond(/how do you feel|how are you feeling/i, function (msg) {
+        var coinFlipHeads = Math.floor(Math.random() * 2);
+        var messagePair = (coinFlipHeads) ? ["I feel.", "I..."] :
+            ["I feel", "strange."];
+        msg.send(messagePair[0]);
+        setTimeout(function () {
+            msg.send(messagePair[1]);
+        }, 1000);
     });
 }
