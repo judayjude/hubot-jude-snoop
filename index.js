230dbf7a4d732704e51f5b2cac04528f8f73ff9f
Jude Solis <jude.solis@service-now.com>
2015-12-01 15:02:32 -0800

    fix 'your mom is...' triggered by 'you are something..' messages

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ module.exports = function (robot) {
         }
     });
 
-    robot.hear(/you(?: a|'?)re (a|the) ([^\.\?!]+)(?:!|\.|$)/i, function (msg) {
+    robot.hear(/you(?: a|'?)re (a |the )([^\.\?!]+)(?:!|\.|$)/i, function (msg) {
         var article = msg.match[1];
         var insinuation = msg.match[2];
         setTimeout(function () {
